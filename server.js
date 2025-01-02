9f0d071303d708fd4467127f167f91bec84c5b98
Gestures5 <kenshimadax@gmail.com>
2025-01-02 21:11:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '/index.html')));
 
 // Serve index.html when accessing the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
