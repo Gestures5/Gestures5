f437325a20179462574d06c4f32a2e20a1597113
Gestures5 <kenshimadax@gmail.com>
2025-03-13 00:09:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html', 'style.css', 'script.js'));
+    res.sendFile(path.join(__dirname, 'index.html', 'styles.css', 'script.js'));
 });
 
 app.listen(PORT, () => {
